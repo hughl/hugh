a53ce1cc7f0bebeb747fe7909b6e823c9ae04483
Hugh <hughphone@gmail.com>
2018-03-10 14:30:05 +0000

    Fixes after comment on initial commit

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -1,24 +1,24 @@
 pragma solidity ^0.4.0;
 contract Splitter {
     
-    mapping (address => uint) public payeeBalances;
+    mapping (address => uint) public balances;
     
-    event LogWithdrawal(address withdrawer, uint amountWithDrawn);
-    event LogSplitEther(address from, uint value, address payeeOne, address payeeTwo);
+    event LogWithdrawal(address indexed withdrawer, uint amountWithDrawn);
+    event LogSplitEther(address indexed from, uint value, address  indexed payeeOne, address indexed payeeTwo);
     
     function splitEther(address payeeOne, address payeeTwo) public payable {
         require(msg.value > 0);
         uint etherValueHalf = msg.value / 2;
-        payeeBalances[payeeOne] += etherValueHalf;
-        payeeBalances[payeeTwo] += etherValueHalf;
+        balances[payeeOne] += etherValueHalf;
+        balances[payeeTwo] += etherValueHalf;
         LogSplitEther(msg.sender, msg.value, payeeOne, payeeTwo);
     }    
     
     function withdraw() public {
-        uint amount = payeeBalances[msg.sender];
+        uint amount = balances[msg.sender];
         require(amount > 0);
-        payeeBalances[msg.sender] = 0;
-        msg.sender.transfer(amount);
+        balances[msg.sender] = 0;
         LogWithdrawal(msg.sender, amount);
+        msg.sender.transfer(amount);
     }
 }
--- splitter/test/Splitter.js
+++ splitter/test/Splitter.js
@@ -1,54 +1,81 @@
+
 var SplitterContract = artifacts.require("./Splitter.sol");
+Promise = require("bluebird");
+Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 
 contract('Splitter', function(accounts) {
-    var contract;
+    var splitterContract;
     /** The account that will send ether to be split */
-    var account_sender = accounts[0];
+    const account_sender = accounts[0];
     /** An account that will be specified as a payee in the splitEther contract function */ 
-    var account_payee1 = accounts[1];
+    const account_payee1 = accounts[1];
     /** Another account that will be specified as a payee in the splitEther contract function */ 
-    var account_payee2 = accounts[2];
+    const account_payee2 = accounts[2];
+
+    const contractCreator = accounts[3];
 
     beforeEach('setup contract for each test', function () {
-        return SplitterContract.new().then(function(instance) {
+        return SplitterContract.new({from:contractCreator}).then(function(instance) {
             console.log("created new contract");
-            contract = instance;
+            splitterContract = instance;
         });
     });
-
+  
   it("should split ether", function() {
-    return contract.splitEther(account_payee1, account_payee2, {from: account_sender, value: 10}).then(
-        (txn) => {
-            return contract.payeeBalances.call(account_payee1);
+    return splitterContract.splitEther(account_payee1, account_payee2, {from: account_sender, value: 10}).then(
+        (result) => {
+            var splitEtherEvent = result.logs[0];
+            assert.equal(result.logs.length, 1);
+            assert.equal(splitEtherEvent.args.from, account_sender);
+            assert.equal(splitEtherEvent.args.value, 10);
+            assert.equal(splitEtherEvent.args.payeeOne, account_payee1);
+            assert.equal(splitEtherEvent.args.payeeTwo, account_payee2); 
+
+            return splitterContract.balances(account_payee1);
         } ).then(amount =>  {
+            console.log("amount = " + JSON.stringify(amount));
             assert.equal(amount, 5, "Invalid amount transferred to payee1");
             
-            return contract.payeeBalances.call(account_payee1);
+            return splitterContract.balances.call(account_payee2);
         }).then(amount =>  {
             assert.equal(amount, 5, "Invalid amount transferred to payee2");
         });;
     });
 
     it("should allow accounts to withdraw split ether", async() => {
-        const payee1InitialBalance = await web3.eth.getBalance(account_payee1);
-        const payee2InitialBalance = await web3.eth.getBalance(account_payee2);
+        const payee1InitialBalance = await web3.eth.getBalancePromise(account_payee1);
+        const payee2InitialBalance = await web3.eth.getBalancePromise(account_payee2);
         const ethToSplit = web3.toWei(2, "ether");
 
-        await contract.splitEther(account_payee1, account_payee2, {from: account_sender, value: ethToSplit });
+        await splitterContract.splitEther(account_payee1, account_payee2, {from: account_sender, value: ethToSplit });
 
-        var payee1TxObj = await contract.withdraw({from: account_payee1});
+        // payee 1 withdraw
+        var payee1TxObj = await splitterContract.withdraw({from: account_payee1});
+        var withdrawalEvent = payee1TxObj.logs[0];
+        // check for withdrawal event
+        assert.equal(payee1TxObj.logs.length, 1);
+        assert.equal(withdrawalEvent.args.withdrawer, account_payee1);
+        assert.equal(withdrawalEvent.args.amountWithDrawn, ethToSplit / 2);
+
         var payee1ExpectedBalance =  await getExpectedBalanceAfterWithdraw(account_payee1, payee1InitialBalance, payee1TxObj.receipt, ethToSplit);
         var payee1NewBalance = await web3.eth.getBalance(account_payee1);
 
-        var payee2TxObj = await contract.withdraw({from: account_payee2});
+        // payee 2 withdraw
+        var payee2TxObj = await splitterContract.withdraw({from: account_payee2});
+        var withdrawalEvent2 = payee2TxObj.logs[0];
+        // check for withdrawal event
+        assert.equal(payee2TxObj.logs.length, 1);
+        assert.equal(withdrawalEvent2.args.withdrawer, account_payee2);
+        assert.equal(withdrawalEvent2.args.amountWithDrawn, ethToSplit / 2);
+
         var payee2ExpectedBalance =  await getExpectedBalanceAfterWithdraw(account_payee2, payee2InitialBalance, payee2TxObj.receipt, ethToSplit);
         var payee2NewBalance = await web3.eth.getBalance(account_payee2);
 
         console.log ("payee1  initialBalance: " + payee1InitialBalance + ", newBalance:" + payee1NewBalance + ", expected: " + payee1ExpectedBalance);
         console.log ("payee2 initBalance: " + payee1InitialBalance + ", newBalance: " + payee2NewBalance + ", expected: " + payee2ExpectedBalance);
 
-        assert.equal("" +payee1NewBalance, "" +payee1ExpectedBalance, "Payee1 balance incorrect after split");
-        assert.equal("" +payee2NewBalance, "" +payee2ExpectedBalance, "Payee2 balance incorrect after split");
+        assert.equal(payee1NewBalance.toString[10], payee1ExpectedBalance.toString[10], "Payee1 balance incorrect after split");
+        assert.equal(payee2NewBalance.toString[10], payee2ExpectedBalance.toString[10], "Payee2 balance incorrect after split");
     });
 
     /**
@@ -60,7 +87,7 @@ contract('Splitter', function(accounts) {
      * @param {*} amountSplit the amount of ether that was originally split
      */
     async function getExpectedBalanceAfterWithdraw (address, initialBalance, withDrawTransactionReceipt, amountSplit) {
-        var tx = await web3.eth.getTransaction(withDrawTransactionReceipt.transactionHash);
+        var tx = await web3.eth.getTransactionPromise(withDrawTransactionReceipt.transactionHash);
         var gasUsed = web3.toBigNumber(withDrawTransactionReceipt.gasUsed);
         var gasCost = tx.gasPrice.times(gasUsed);
         console.log("gas cost: " + gasCost);
@@ -75,7 +102,7 @@ contract('Splitter', function(accounts) {
         const payee1InitialBalance = await web3.eth.getBalance(account_payee1);
         var err = null;
         try {
-            await contract.withdraw({from: account_payee1});
+            await splitterContract.withdraw({from: account_payee1});
           } catch (error) {
             err = error
           }
